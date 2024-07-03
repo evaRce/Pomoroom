a4d8ccd9e0e0036b6373e552d1c85768461c005a
evaRce <eva.arce123@hotmail.com>
2024-07-03 22:03:34 +0200

    Añade behaviour Presence

---
+++ lib/pomoroom_web/channels/presence.ex
@@ -0,0 +1,11 @@
+defmodule PomoroomWeb.Presence do
+  @moduledoc """
+  Provides presence tracking to channels and processes.
+
+  See the [`Phoenix.Presence`](https://hexdocs.pm/phoenix/Phoenix.Presence.html)
+  docs for more details.
+  """
+  use Phoenix.Presence,
+    otp_app: :pomoroom,
+    pubsub_server: Pomoroom.PubSub
+end
