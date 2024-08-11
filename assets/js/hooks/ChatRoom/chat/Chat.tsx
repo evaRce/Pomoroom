ef6f6f128974d74b9d83766c735e63c07e007ca2
evaRce <eva.arce123@hotmail.com>
2024-08-11 17:22:14 +0200

    feat(#7): Implementar desplazamiento automático hacia
    abajo en el chat, para mostrar el mensaje más reciente

--- assets/js/hooks/ChatRoom/chat/Chat.tsx
+++ assets/js/hooks/ChatRoom/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import HeaderChat from "./components/HeaderChat";
 import Message from "./message/Message";
 import FooterChat from "./components/FooterChat";
@@ -7,6 +7,7 @@ import { useEventContext } from "../EventContext";
 export default function Chat() {
   const [messages, setMessages] = useState([]);
   const { getEventData, removeEvent } = useEventContext();
+  const messagesEndRef = useRef(null);
 
   useEffect(() => {
     const msgs = getEventData("show_list_messages");
@@ -28,16 +29,24 @@ export default function Chat() {
     setMessages((prevMessages) => [...prevMessages, message]);
   };
 
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
     <div className="flex flex-col flex-grow w-full border-l border-r">
       <HeaderChat />
       <main
         className="flex flex-col h-[83vh] overflow-y-auto overflow-x-hidden p-5 border-t border-b"
         style={{ scrollbarWidth: "thin" }}
+        ref={messagesEndRef}
       >
         {messages.length > 0 && messages.map((message) => (
           <Message key={message.public_id_msg} message={message} />
         ))}
+        <div ref={messagesEndRef}></div>
       </main>
       <FooterChat addMessage={addMessage} />
     </div>
