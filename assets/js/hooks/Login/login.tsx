38fd9c136390763a2bdf5e0699b390945a10fba5
evaRce <eva.arce123@hotmail.com>
2024-09-06 17:15:50 +0200

    feat: Rediseño de home, login y singup

--- assets/js/hooks/Login/login.tsx
+++ assets/js/hooks/Login/login.tsx
@@ -1,86 +1,124 @@
-import React, { useEffect } from "react";
-import { Alert, Card, Button, Form, Input } from 'antd';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import React, { useEffect, useState } from "react";
+import { Button, Form, Input } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
 
 export interface LoginProps {
   searchUser(email: string, password: string): any;
   errors: object;
 }
 
-export const Login: React.FC<LoginProps> = (props: LoginProps) => { 
-  const {searchUser, errors} = props;
-	const [form] = Form.useForm();
-  
+export const Login: React.FC<LoginProps> = (props: LoginProps) => {
+  const { searchUser, errors } = props;
+  const [form] = Form.useForm();
+  const [imageNumber, setImageNumber] = useState(1);
+
   const onFinish = (newValues: any) => {
-    searchUser(newValues.email, newValues.password)  
+    searchUser(newValues.email, newValues.password);
   };
 
   useEffect(() => {
-    // Set custom backend errors to the form fields
     if (errors) {
-      const commonError = errors['email'] || errors['password'];
+      const commonError = errors["email"] || errors["password"];
       if (commonError) {
         form.setFields([
-          { name: 'email', errors: [commonError] },
-          { name: 'password', errors: [commonError] },
+          { name: "email", errors: [commonError] },
+          { name: "password", errors: [commonError] },
         ]);
       }
     }
   }, [errors, form]);
-  
+
+  useEffect(() => {
+    const randomImageNumber = Math.floor(Math.random() * 5) + 1;
+    setImageNumber(randomImageNumber);
+  }, []);
+
   return (
-    <Card style={{ width: 450 }}>
-      <h1 style={{textAlign: 'center'}}>¡Nos alegra verte otra vez! Ingresa tus datos para comenzar.</h1>
-      <Form
-        form={form}
-        layout="vertical"
-        name="normal_login"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        style={{ maxWidth: 400 }}
-      >
-        <Form.Item
-          label="Email" 
-          name="email"
-          rules={[
-            { required: true, message: '¡Por favor ingrese su correo electrónico!' }
-          ]}
-        >
-          <Input 
-            prefix={<UserOutlined className="site-form-item-icon" />} 
-            placeholder="" />
-        </Form.Item>
-        <Form.Item
-          label="Contraseña"
-          name="password"
-          rules={[
-            { required: true, message: '¡Por favor ingrese su contraseña!' }
-          ]}
-        >
-          <Input.Password
-            prefix={<LockOutlined className="site-form-item-icon" />}
-          />
-        </Form.Item>
-        <Form.Item>
-          <a className="login-form-forgot" href="retrieve-password">
-            Olvide mi contraseña
-          </a>
-        </Form.Item>
-        <Form.Item>
-          <Button 
-            htmlType="submit" 
-            className="login-form-button"
-            style={{ borderColor: "#fdba74" }}
-            block
-          >
-            Iniciar sesión
-          </Button>
-          <br></br>
-          <a href="signup">
-            <h1 style={{textAlign: 'center'}}>¿No tienes una cuenta?</h1>
-          </a>
-        </Form.Item>
-      </Form>
-    </Card>
+    <div
+      className="min-h-screen flex flex-col items-center justify-center p-8"
+      style={{
+        backgroundImage: `url(/images/background2/background-${imageNumber}.svg)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundColor: "rgba(255, 255, 255, 0.5)",
+        backgroundBlendMode: "overlay",
+      }}
+    >
+      <div className="max-w-md w-full">
+        <div className="p-5 rounded-2xl bg-white shadow">
+          <p className="text-gray-800 text-center text-2xl font-bold">
+            ¡Nos alegra verte otra vez!
+            <p className="text-gray-800 text-center text-xl font-bold">
+              Ingresa tus datos para comenzar.
+            </p>
+          </p>
+          <Form
+              form={form}
+              layout="vertical"
+              name="normal_login"
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+              className="mt-5 space-y-4"
+            >
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "¡Por favor ingrese su correo electrónico!",
+                  },
+                ]}
+              >
+                <Input
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder=""
+                />
+              </Form.Item>
+              <Form.Item
+                label="Contraseña"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "¡Por favor ingrese su contraseña!",
+                  },
+                ]}
+              >
+                <Input.Password
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                />
+              </Form.Item>
+              <Form.Item>
+                <a
+                  href="retrieve-password"
+                  className="text-blue-600 hover:underline font-semibold"
+                >
+                  Olvidaste tu contraseña?
+                </a>
+              </Form.Item>
+              <Form.Item>
+                <Button
+                  htmlType="submit"
+                  className="text-white text-sm font-semibold transitiona-all duration-700 bg-blue-500 "
+                  block
+                >
+                  Iniciar sesión
+                </Button>
+                <p className="text-gray-800 text-sm !mt-5 mb-0 text-center">
+                  ¿No tienes una cuenta?
+                  <a
+                    href="signup"
+                    className="text-blue-600 hover:underline ml-1 whitespace-nowrap font-semibold"
+                  >
+                    Registrate aquí
+                  </a>
+                </p>
+              </Form.Item>
+            </Form>
+        </div>
+      </div>
+    </div>
   );
+  
 };
--- assets/js/hooks/SignUp/signup.tsx
+++ assets/js/hooks/SignUp/signup.tsx
@@ -1,123 +1,183 @@
-import React, { useEffect } from "react";
-import { Card, Button, Form, Input } from 'antd';
-import { LockOutlined, UserOutlined, RobotOutlined } from '@ant-design/icons';
+import React, { useEffect, useState } from "react";
+import { Button, Form, Input } from "antd";
+import { LockOutlined, UserOutlined, RobotOutlined } from "@ant-design/icons";
 
 export interface SignUpProps {
-	submitUser(newUsername: string, newPassword: string, newNickname: string): any;
-	errors: object;
+  submitUser(
+    newUsername: string,
+    newPassword: string,
+    newNickname: string
+  ): any;
+  errors: object;
 }
 
 export const SignUp: React.FC<SignUpProps> = (props: SignUpProps) => {
-	const { submitUser, errors } = props;
-	const [form] = Form.useForm();
-	const nicknameRegex = new RegExp(/^\w[\w.]{2,18}\w$/);
+  const { submitUser, errors } = props;
+  const [form] = Form.useForm();
+  const nicknameRegex = new RegExp(/^\w[\w.]{2,18}\w$/);
+	const [imageNumber, setImageNumber] = useState(1);
 
-	const onFinish = (newValues: any) => {
-		submitUser(newValues.email, newValues.confirmPassword, newValues.nickname)
-	};
+  const onFinish = (userData: any) => {
+    submitUser(userData.email, userData.confirmPassword, userData.nickname);
+  };
 
-	useEffect(() => {
-    // Set custom backend errors to the form fields
+  useEffect(() => {
     if (errors) {
-      form.setFields(Object.keys(errors).map(key => ({
-        name: key,
-        errors: [errors[key]],
-      })));
+      form.setFields(
+        Object.keys(errors).map((key) => ({
+          name: key,
+          errors: [errors[key]],
+        }))
+      );
     }
   }, [errors, form]);
 
-	return (
-		<Card style={{ width: 450 }}>
-			<h1 style={{textAlign: 'center'}}>¡Es fácil empezar! Crea tu cuenta y empieza a ser más productivo.</h1>
-			<Form
-				form={form}
-				layout="vertical"
-				name="normal_signup"
-				onFinish={onFinish}
-				style={{ maxWidth: 400 }}
-				scrollToFirstError
-			>
-			<Form.Item
-				label="Email"
-				name="email"
-				rules={[
-					{ required: true, message: '¡Por favor ingrese su email!' },
-					{ type: 'email', message: '¡La entrada no es un email válido!' }
-				]}
-				hasFeedback
-			>
-				<Input 
-					prefix={<UserOutlined className="site-form-item-icon" />} 
-				/>
-			</Form.Item>
+	useEffect(() => {
+    const randomImageNumber = Math.floor(Math.random() * 5) + 1;
+    setImageNumber(randomImageNumber);
+  }, []);
 
-			<Form.Item
-				label="Contraseña"
-				name="password"
-				rules={[
-					{ required: true, 	message: '¡Por favor ingrese su contraseña!' },
-					{ type: 'string', min: 8, max: 64, message: '¡La contraseña debe tener entre 8 y 64 caracteres!' }
-				]}
-				hasFeedback
-			>
-				<Input.Password 
-					prefix={<LockOutlined className="site-form-item-icon" />}
-				/>
-			</Form.Item>
+  return (
+    <div
+      className="min-h-screen flex flex-col items-center justify-center p-8"
+      style={{
+        backgroundImage: `url(/images/background2/background-${imageNumber}.svg)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+				backgroundColor: "rgba(255, 255, 255, 0.5)",
+        backgroundBlendMode: "overlay",
+      }}
+    >
+      <div className="max-w-md w-full">
+        <div className="p-5 rounded-2xl bg-white shadow">
+          <p className="text-gray-800 text-center text-2xl font-bold">
+            ¡Es fácil empezar!
+						<p className="text-gray-800 text-center text-xl font-bold">
+            	Crea tu cuenta y empieza a ser más productivo.
+						</p>
+          </p>
+          <Form
+            form={form}
+            layout="vertical"
+            name="normal_signup"
+            onFinish={onFinish}
+            className="mt-5 space-y-4"
+            scrollToFirstError
+          >
+            <Form.Item
+              label="Email"
+							className="mb-2"
+              name="email"
+              rules={[
+                { required: true, message: "¡Por favor ingrese su email!" },
+                {
+                  type: "email",
+                  message: "¡La entrada no es un email válido!",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+              />
+            </Form.Item>
 
-			<Form.Item
-				label="Confirmar contraseña"
-				name="confirmPassword"
-				dependencies={['password']}
-				rules={[
-					{ required: true, message: '¡Por favor, confirme su contraseña!' },
-					({ getFieldValue }) => ({
-						validator(_, value) {
-						if (!value || getFieldValue('password') === value) {
-							return Promise.resolve();
-						}
-						return Promise.reject(new Error('¡La contraseña que ingresó no coincide con la anterior!'));
-						},
-					}),
-				]}
-				hasFeedback
-			>
-				<Input.Password 
-					prefix={<LockOutlined className="site-form-item-icon" />}
-				/>
-			</Form.Item>
+            <Form.Item
+              label="Contraseña"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "¡Por favor ingrese su contraseña!",
+                },
+                {
+                  type: "string",
+                  min: 8,
+                  max: 64,
+                  message: "¡La contraseña debe tener entre 8 y 64 caracteres!",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+              />
+            </Form.Item>
 
-			<Form.Item
-				label="Apodo"
-				name="nickname"
-				tooltip="¿Cómo quieres que te llamen los demás?"
-				rules={[
-					{ required: true, message: '¡Por favor ingresa tu apodo!', whitespace: true },
-					{ pattern: nicknameRegex, message: '¡La entrada no es un apodo válido!' },
-					{ min: 2, max: 64, message: 'El apodo debe tener entre 2 y 64 caracteres' }
-				]}
-				hasFeedback
-			>
-				<Input 
-					prefix={<RobotOutlined />}
-				/>
-			</Form.Item>
+            <Form.Item
+              label="Confirmar contraseña"
+              name="confirmPassword"
+              dependencies={["password"]}
+              rules={[
+                {
+                  required: true,
+                  message: "¡Por favor, confirme su contraseña!",
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error(
+                        "¡La contraseña que ingresó no coincide con la anterior!"
+                      )
+                    );
+                  },
+                }),
+              ]}
+              hasFeedback
+            >
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+              />
+            </Form.Item>
 
-			<Form.Item>
-				<Button 
-					htmlType="submit" 
-					className="signup-form-button"
-					style={{ borderColor: "#fdba74" }}
-					block
-				>
-					Crear cuenta
-				</Button>
-				<br></br>
-				<a href="login">
-					<h1 style={{textAlign: 'center'}}>Ya tengo una cuenta</h1>
-				</a>
-			</Form.Item>
-			</Form>
-		</Card>
-	);
+            <Form.Item
+              label="Apodo"
+              name="nickname"
+              tooltip="¿Cómo quieres que te llamen los demás?"
+              rules={[
+                {
+                  required: true,
+                  message: "¡Por favor ingresa tu apodo!",
+                  whitespace: true,
+                },
+                {
+                  pattern: nicknameRegex,
+                  message: "¡La entrada no es un apodo válido!",
+                },
+                {
+                  min: 2,
+                  max: 64,
+                  message: "El apodo debe tener entre 2 y 64 caracteres",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input prefix={<RobotOutlined />} />
+            </Form.Item>
+						<Form.Item>
+						<Button
+              htmlType="submit"
+              className="mt-2 text-white text-sm font-semibold transitiona-all duration-700 bg-blue-500"
+              block
+            >
+              Crear cuenta
+            </Button>
+            <p className="text-gray-800 text-sm !mt-5 mb-0 text-center">
+              Ya
+              <a
+                href="login"
+                className="text-blue-600 hover:underline ml-1 whitespace-nowrap font-semibold"
+              >
+                tengo una cuenta
+              </a>
+            </p>
+						</Form.Item>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 };
